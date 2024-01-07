0654ce61a7959a3987e64ee3ae400fcf420187d2
romangurung <>
2024-01-07 14:33:28 -0800

    project setup

---
+++ moneyXchanger/moneyXchangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  moneyXchangerApp.swift
+//  moneyXchanger
+//
+//  Created by Roman Gurung on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct moneyXchangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
